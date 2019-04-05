9fa7c5868ed233cecd462f35240b0a25cde0fc93
tltyogg <saralyn.carbaugh@gmail.com>
2019-04-05 09:27:09 -0400

    adding comments
    Signed-off-by: tltyogg <saralyn.carbaugh@gmail.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
+/*
+    This is a class comment. This program is intended to show how comments work.
+ */
+
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) { //Just need to remember this line to start the main class.
+        System.out.println("Hello World!"); // Prints out "Hello World!" into the terminal.
     }
 }
